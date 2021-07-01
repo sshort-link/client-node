61a39435363ca2b07c67c234b94ec2aa0a38b9e9
Yusuke Ueno <yusuke1581@gmail.com>
2021-07-02 00:13:53 +0900

    Chore/docs (#2)
    * add condition for deploy
    
    * update README.md
    
    * add example code
    
    * update and add comments

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,44 @@ import { ShortenOptions, SShortLink, SShortLinkConfig } from "./client";
 
 const sshortLink = SShortLink.create();
 
-export const init = (config: SShortLinkConfig = {}) => {
-  sshortLink.updateConfig(config);
+type InitConfig = SShortLinkConfig & {
+  accessToken?: string;
 };
 
+/**
+ * The SShort Link Node SDK Client.
+ *
+ * To use this SDK, call the {@link init} function as early as possible in the
+ * main entry module. To set context information or send manual events, use the
+ * provided methods.
+ *
+ * @example
+ * ```
+ *
+ * import { init } from '@sshort-link/client-node';
+ *
+ * init({
+ *   accessTokne: '__ACCESS_TOKEN__',
+ *   // ...
+ * });
+ * ```
+ */
+export const init = (config: InitConfig = {}) => {
+  const headers: Record<string, string> = {};
+  const accessToken =
+    config.accessToken || process.env["SSHORT_LINK_ACCESS_TOKEN"];
+  if (accessToken) {
+    headers["Authorization"] = `Bearer ${accessToken}`;
+  }
+  sshortLink.updateConfig({
+    ...config,
+    headers: config.headers || headers,
+  });
+};
+
+/**
+ * Returns a shorten url.
+ */
 export const shorten = async (
   longURL: string,
   options: ShortenOptions = {}
